9586e843150b357aa58146345403fe6c12057a9a
wmlutz <william.meany.lutz@gmail.com>
2017-06-06 11:58:01 -0400

    Added grab woodpecker config key

--- daily_subscription_grab.rb
+++ daily_subscription_grab.rb
@@ -6,6 +6,9 @@ puts "Started"
 logger = Logger.new("#{File.dirname(__FILE__)}/etc/log.log", 0, 100 * 1024 * 1024)
 logger.level = Logger::DEBUG
 
-puts recent_newsletter_list
+# turn on when ready to actuall do api calls from pardot
+# news_list = recent_newsletter_list
+
+puts grab_woodpecker_config
 
 logger.info("End of run -----------------------------")
