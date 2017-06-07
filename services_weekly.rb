7cfffee69a6886ccb6a32df5661c08942afeb67f
wmlutz <william.meany.lutz@gmail.com>
2017-06-07 18:43:13 -0400

    Train update

--- services_weekly.rb
+++ services_weekly.rb
@@ -20,6 +20,45 @@ def grab_salesforce_config
       end
     end
   end
-  logger.info("got config for #{config[:user_name]}")
+  logger.info("got config for #{config['user_name']}")
   config
+end
+
+# gets the contacts from SFDC
+def grab_SFDC_contacts
+  logger = Logger.new("#{File.dirname(__FILE__)}/etc/weekly.log", 0, 100 * 1024 * 1024)
+  logger.level = Logger::DEBUG
+
+  # Get Salesforce config info
+  config = grab_salesforce_config
+
+  # Authenticate Salesforce connection
+  begin
+    client = Restforce.new(username: config[:username],
+                           password: config[:password],
+                           security_token: config[:security_token],
+                           client_id: config[:client_id],
+                           client_secret: config[:client_secret],
+                           api_version: '38.0')
+  rescue g
+    logger.info("failed to authenticate SFDC: #{g}")
+  end
+
+  # Get the contacts between 120 and 127 days since last activity
+  begin
+    contacts = client.query("SELECT FirstName,LastName,email,TR1__Work_Email__c,TR1__Secondary_Email__c,MKT_Personal_Email__c,Last_Activity_Date__c FROM contact WHERE Last_Activity_Date__c < N_DAYS_AGO:120 AND Last_Activity_Date__c > N_DAYS_AGO:128 ORDER BY Last_Activity_Date__c ASC")
+    logger.info("Got #{contacts.length} contacts.")
+    logger.info("grabbed contacts: #{contacts}")
+
+    # below for testing only, to remove later XXXXXXXXXXX
+    num = 1
+    contacts.each do |contact|
+      logger.info("Contact #{num}: #{contact['FirstName']}")
+      num = num + 1
+    end
+  rescue h
+    logger.info("Failed to grab query: #{h}")
+  end
+
+  contacts
 end
--- weekly_old_grab.rb
+++ weekly_old_grab.rb
@@ -5,30 +5,12 @@ require_relative 'services_weekly'
 logger = Logger.new("#{File.dirname(__FILE__)}/etc/weekly.log", 0, 100 * 1024 * 1024)
 logger.level = Logger::DEBUG
 
-# Get Salesforce config info
-config = grab_salesforce_config
-
-# Authenticate Salesforce connection
-begin
-  client = Restforce.new(username: config[:username],
-                         password: config[:password],
-                         security_token: config[:security_token],
-                         client_id: config[:client_id],
-                         client_secret: config[:client_secret],
-                         api_version: '38.0')
-  logger.info("Trying describe: #{client.describe('Account')}")
-rescue g
-  logger.info("failed to authenticate SFDC: #{g}")
-end
-
-# Get the contacts between 120 and 127 days since last activity
-begin
-  contacts = client.query("SELECT FirstName,LastName,email,Last_Activity_Date__c FROM contact WHERE Last_Activity_Date__c < N_DAYS_AGO:120 AND Last_Activity_Date__c > N_DAYS_AGO:128 ORDER BY Last_Activity_Date__c ASC")
-rescue h
-  logger.info("Failed to grab query: #{h}")
-end
-
-puts contacts
+puts "Starting . . ."
+# Get contacts from SFDC
+contacts = grab_SFDC_contacts
+# logger.info("Contacts: #{contacts}")
 
 # format for woodpecker
 # send that list to woodpecker
+
+puts "And we're done."
