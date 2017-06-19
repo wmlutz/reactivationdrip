da64714694747d25247b10d20d55dee8655fe879
wmlutz <william.meany.lutz@gmail.com>
2017-06-19 09:47:36 -0400

    Added put lines for error checking

--- daily_subscription_grab.rb
+++ daily_subscription_grab.rb
@@ -7,16 +7,20 @@ logger = Logger.new("#{File.dirname(__FILE__)}/etc/daily.log", 0, 100 * 1024 * 1
 logger.level = Logger::DEBUG
 
 logger.info("Beginning of run ----------------")
-
+puts "Getting pardot newsletter list #{DateTime.now}"
 news_list = grab_newsletter_list # gets the most recent newsletter list
+puts "Getting woodies list #{DateTime.now}"
 woodies = grab_woodies # Gets the woodies
+puts "Getting SFDC recents list #{DateTime.now}"
 recent_SFDC = grab_recent_SFDCs
 blacklist = []
 
+puts "Checking Pardot newsletter list to woodies list #{DateTime.now}"
 news_list.each do |prospect| # checks each newsltr entry for being in woodpecker
   blacklist << prospect unless woodies.select { |w| w['email'].downcase! == prospect.downcase! }.empty?
 end
 
+puts "Checking recent SFDC list to woodies list #{DateTime.now}"
 recent_SFDC.each do |email| # checks recent activity SFDC contacts for being in wp
   blacklist << email unless woodies.select { |y| y['email'].downcase! == email.downcase! }.empty?
 end
@@ -24,6 +28,7 @@ end
 logger.info("blacklist: #{blacklist}")
 logger.info("Blacklist length: #{blacklist.length}")
 
+puts "Sending blacklist to supression #{DateTime.now}"
 if blacklist.length > 0
   logger.info("Sending #{blacklist.length} entries to blacklist")
   payload = turn_into_JSONHashArray(blacklist) # formats the new blacklist additions for RESTAPI
